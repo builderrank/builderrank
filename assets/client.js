29382eb54f8324063c79fdacc3df18cb041eb454
builderrank <kwgamez@gmail.com>
2026-06-02 20:46:21 -0600

    Hide reset action when signed in

--- assets/client.js
+++ assets/client.js
@@ -682,6 +682,8 @@ async function refreshAuthState() {
         : "Your reports will save to this account.";
     }
     if (accountLoginButton) accountLoginButton.disabled = true;
+    if (accountResetOpenButton) accountResetOpenButton.hidden = true;
+    if (reportResetOpenButton) reportResetOpenButton.hidden = true;
     if (accountCreateOpenButton) accountCreateOpenButton.hidden = true;
     if (reportCreateOpenButton) reportCreateOpenButton.hidden = true;
     if (accountSignOutButton) accountSignOutButton.hidden = false;
@@ -693,6 +695,8 @@ async function refreshAuthState() {
     if (reportAuthTitle) reportAuthTitle.textContent = "Log in before buying a report";
     if (reportAuthSummary) reportAuthSummary.textContent = "Use the same login any time you come back to view purchased reports.";
     if (accountLoginButton) accountLoginButton.disabled = false;
+    if (accountResetOpenButton) accountResetOpenButton.hidden = false;
+    if (reportResetOpenButton) reportResetOpenButton.hidden = false;
     if (accountCreateOpenButton) accountCreateOpenButton.hidden = false;
     if (reportCreateOpenButton) reportCreateOpenButton.hidden = false;
     if (accountSignOutButton) accountSignOutButton.hidden = true;
